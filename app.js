f79cd8814b5475761ffc125d0d4e7ec27d0f8d4f
tf292201 <tf292201@gmail.com>
2024-01-31 16:16:10 -0500

    promises
    promises

--- app.js
+++ app.js
@@ -1,83 +1,81 @@
-let number = 42
-let baseURL = 'https://deckofcardsapi.com/api/deck';
+let favNumber = 42;
+let baseURL = "http://numbersapi.com";
 
-// Part 1
-//1
-async function getNumberFacts() { 
-  let response = await $.getJSON({
-    url: `http://numbersapi.com/${number}?json`
-  });
-  
-  console.log(response);
-}
-// 2
-let nums = [2,4,6,8,9,65,234]
-async function getMultiNumFacts(){
-  let response= await $.getJSON({url: `http://numbersapi.com/${nums}?json`});
-  console.log(response);
-}
-
-//3
-let factNum = 44
-
-async function getFourFacts() {
-  for (let i = 0; i < 4; i++) {
-    try {
-      let response = await $.getJSON(`http://numbersapi.com/${factNum}?json`);
-      console.log(response);
-      $('body').append(`<p>${response.text}</p>`);
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    }
-  }
-}
-
-//Part 2
-//1
+// 1.
+let response = $.getJSON(`${baseURL}/${favNumber}?json`);
+response.then(data => {
+  console.log(data);
+});
 
 
-async function drawCard() {
-  let data = await $.getJSON(`${baseURL}/new/draw/`);
-  let card = data.cards[0]
-  let suit = card.suit;
-  let value = card.value;
-  console.log(`${value.toLowerCase()} of ${suit.toLowerCase()}`);
-}
+// // 2.
+let favNumbers = [22 , 55 ,87];
+$.getJSON(`${baseURL}/${favNumbers}?json`).then(data => {
+  console.log(data);
+});
 
-//2
-async function drawTwoCards() {
-  let firstCardData = await $.getJSON(`${baseURL}/new/draw/`);
-  let deckId = firstCardData.deck_id;
-  let secondCardData = await $.getJSON(`${baseURL}/${deckId}/draw/`);
-  [firstCardData, secondCardData].forEach(card => {
-    let suit = card.cards[0].suit;
-    let value = card.cards[0].value;
+3.
+Promise.all(
+  Array.from({ length: 4 }, () => {
+    return $.getJSON(`${baseURL}/${favNumber}?json`);
+  })
+).then(facts => {
+  facts.forEach(data => $("body").append(`<p>${data.text}</p>`));
+});
+
+$(function() {
+  let baseURL = 'https://deckofcardsapi.com/api/deck';
+
+
+  // Part 2
+  // 1.
+  $.getJSON(`${baseURL}/new/draw/`).then(data => {
+    let { suit, value } = data.cards[0];
     console.log(`${value.toLowerCase()} of ${suit.toLowerCase()}`);
-  })};
+  });
 
-  //3
-async function setup() {
+  // 2.
+  let firstCard = null;
+  $.getJSON(`${baseURL}/new/draw/`)
+    .then(data => {
+      firstCard = data.cards[0];
+      let deckId = data.deck_id;
+      return $.getJSON(`${baseURL}/${deckId}/draw/`);
+    })
+    .then(data => {
+      let secondCard = data.cards[0];
+      [firstCard, secondCard].forEach(function(card) {
+        console.log(
+          `${card.value.toLowerCase()} of ${card.suit.toLowerCase()}`
+        );
+      });
+    });
+
+  // 3.
+  let deckId = null;
   let $btn = $('button');
   let $cardArea = $('#card-area');
 
-  let deckData = await $.getJSON(`${baseURL}/new/shuffle/`);
-  $btn.show().on('click', async function() {
-    let cardData = await $.getJSON(`${baseURL}/${deckData.deck_id}/draw/`);
-    let cardSrc = cardData.cards[0].image;
-    let angle = Math.random() * 90 - 45;
-    let randomX = Math.random() * 40 - 20;
-    let randomY = Math.random() * 40 - 20;
-    $cardArea.append(
-      $('<img>', {
-        src: cardSrc,
-        css: {
-          transform: `translate(${randomX}px, ${randomY}px) rotate(${angle}deg)`
-        }
-      })
-    );
-    if (cardData.remaining === 0) $btn.remove();
+  $.getJSON(`${baseURL}/new/shuffle/`).then(data => {
+    deckId = data.deck_id;
+    $btn.show();
   });
-}
-setup();
 
-
+  $btn.on('click', function() {
+    $.getJSON(`${baseURL}/${deckId}/draw/`).then(data => {
+      let cardSrc = data.cards[0].image;
+      let angle = Math.random() * 90 - 45;
+      let randomX = Math.random() * 40 - 20;
+      let randomY = Math.random() * 40 - 20;
+      $cardArea.append(
+        $('<img>', {
+          src: cardSrc,
+          css: {
+            transform: `translate(${randomX}px, ${randomY}px) rotate(${angle}deg)`
+          }
+        })
+      );
+      if (data.remaining === 0) $btn.remove();
+    });
+  });
+});
